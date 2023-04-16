c23fb0bca308ed810db97f867e6f4c0a6560be46
dawjus <dawid.justyna@op.pl>
2023-04-16 23:55:37 +0200

    add button amount and fix bugs

--- Scraping/WebFlashscore.py
+++ Scraping/WebFlashscore.py
@@ -8,48 +8,55 @@ from datetime import datetime
 import pandas as pd
 import time
 import math
+import re
 
-def flashscore(path='https://www.flashscore.com', deltaDate =0):
+def flashscore(path='https://www.flashscore.com', deltaDate =0, amount=5):
     op = webdriver.ChromeOptions()
     op.add_argument('headless')
     op.add_argument("window-size=1920,1080")  #1920x1080
     browser = webdriver.Chrome("C:/Users/dawid/PycharmProjects/FlaskProject/chromedriver.exe", options=op)
     browser.get(path)
-    #browser.implicitly_wait(7)
     #browser.find_element(By.XPATH, "//*[contains(text(), 'I Accept')]").click()
     WebDriverWait(browser, 10).until(
         EC.element_to_be_clickable((By.XPATH, '//*[contains(text(), "I Accept")]'))
     ).click()
+    WebDriverWait(browser,10).until(
+        EC.element_to_be_clickable((By.XPATH, "//*[@id='calendarMenu']"))).click()
+    #browser.find_element(By.XPATH, "//*[@id='calendarMenu']").click()
     #browser.implicitly_wait(10)
-    time.sleep(3)
-    browser.find_element(By.XPATH, "//*[@id='calendarMenu']").click()
-    #browser.implicitly_wait(10)
-    time.sleep(5)
+    WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "calendar__listItem"))
+    )
     browser.find_elements(By.CLASS_NAME, "calendar__listItem")[deltaDate+7].click()
     #browser.implicitly_wait(10)
-    time.sleep(5)
+   # time.sleep(5)
     browser.switch_to.window(browser.window_handles[-1])
-    time.sleep(5)
+    WebDriverWait(browser,10).until(
+        EC.presence_of_element_located((By.XPATH,
+                                     '//div[@class="event__match event__match--scheduled event__match--twoLine"]')))
     elements = browser.find_elements(By.XPATH,
                                      '//div[@class="event__match event__match--scheduled event__match--twoLine"]')
-    browser.implicitly_wait(20)
+
     counter = 0
     matches = []  # (name, [courses home], [courses away], [courses draw])
     for element in elements:
-        if counter > 3:
+        if counter >= amount:
             break
         browser.execute_script("arguments[0].click();", element)
         browser.implicitly_wait(10)
         browser.switch_to.window(browser.window_handles[-1])
         browser.implicitly_wait(10)
-        Odds_click = browser.find_element(By.XPATH, "//*[contains(text(), 'Odds')]")
-        browser.execute_script("arguments[0].click();", Odds_click)
+        #Odds_click = browser.find_element(By.XPATH, "//*[contains(text(), 'Odds')]")
+        Odds_click = browser.find_elements(By.XPATH, "//*[contains(text(), 'Odds')]")
+        if len(Odds_click) > 1:
+            browser.execute_script("arguments[0].click();", Odds_click[0])
+        else:
+            browser.close()
+            browser.switch_to.window(browser.window_handles[0])
+            continue
+        #browser.execute_script("arguments[0].click();", Odds_click)
         odds = browser.find_elements(By.CLASS_NAME, 'oddsCell__odd')
-        matches.append([browser.title])
-        matches[counter].append([])
-        matches[counter].append([])
-        matches[counter].append([])
-        matches[counter].append(0)
+        matches.append([browser.title, [], [], [], 0])
         mod_ = 0
         for odd in odds:
             if mod_ % 3 == 0:
@@ -62,8 +69,6 @@ def flashscore(path='https://www.flashscore.com', deltaDate =0):
         counter += 1
         browser.close()
         browser.switch_to.window(browser.window_handles[0])
-
-    #browser.implicitly_wait(5)
     browser.close()
     stringCut(matches)
     probability(matches)
@@ -72,14 +77,15 @@ def flashscore(path='https://www.flashscore.com', deltaDate =0):
 
 
 def stringCut(arr):
-    key = '»'
     for i in range(len(arr)):
         for j in range(1, 4):
             arr[i][j] = list(filter(lambda x: x != '', arr[i][j]))
             for k in range(len(arr[i][j])):
-                if key in arr[i][j][k]:
-                    arr[i][j][k] = arr[i][j][k][7:]
-                    arr[i][j][k] = float(arr[i][j][k])
+                arr[i][j][k] = re.findall(r'\d+\.\d+', arr[i][j][k])
+                if len(arr[i][j][k]) >1:
+                    arr[i][j][k] = float(arr[i][j][k][1])
+                else:
+                    arr[i][j][k] =-1
 
 def probability(arr):
     for i in range(len(arr)):
--- app.py
+++ app.py
@@ -1,40 +1,66 @@
-from flask import Flask, render_template_string, request, render_template, Response
+from flask import Flask, render_template_string, request, render_template, Response, redirect, session
 import pandas as pd
 from Scraping import WebFlashscore
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 import datetime
+from werkzeug.utils import secure_filename
+
+
 app = Flask(__name__)
 
+app.secret_key = 'supersecretkey'
+ALLOWED_EXTENSIONS = {'csv'}
 
 @app.route('/', methods=[ 'GET','POST'])
-def hello_world():
-    today = datetime.date.today()
+def start():
     if request.method =='POST':
-        selectDateString = request.form.get('selected_date')
-        selectDate = datetime.datetime.strptime(selectDateString, '%Y-%m-%d').date()
-        delta = (selectDate - today).days
-        if 'soccer' in request.form:
-            df = WebFlashscore.flashscore('https://www.flashscore.com', delta)
-        elif 'basketball' in request.form:
-            df = WebFlashscore.flashscore('https://www.flashscore.com/basketball/', delta)
-        elif 'handball' in request.form:
-            df = WebFlashscore.flashscore('https://www.flashscore.com/handball/', delta)
-        else:
-            df = 0
-
-        html_string = df.to_html(classes='table table-striped', index=False)
-        html_string = html_string.replace('<table',
-                                          '<table style="background-color: lightblue; border: 2px solid black;"')
-        html_string = html_string.replace('<tbody>', '<tbody style="background-color: white;">')
-        html_string = html_string.replace('<body>', '<body bgcolor="#FFFFCC">')
-        return html_string
-
+        selected_date = request.form.get('selected_date')
+        sport = request.form.get('sport')
+        amount = request.form.get('selected_amount')
+        session['selected_date'] = selected_date
+        session['sport'] = sport
+        session['amount'] = amount
+        return redirect('/matches')
     today = datetime.date.today().strftime('%Y-%m-%d')
     max_date = (datetime.date.today() + datetime.timedelta(days=7)).strftime('%Y-%m-%d')
     return render_template('temp.html', today=today, max_date=max_date)
 
 
+
+@app.route('/matches', methods=['GET','POST'])
+def matches():
+    today = datetime.date.today()
+    sport = session.get('sport')
+    selectDateString = session.get('selected_date')
+    amount = int(session.get('amount'))
+    selectDate = datetime.datetime.strptime(selectDateString, '%Y-%m-%d').date()
+    delta = (selectDate - today).days
+    if 'Soccer' == sport: #request.form:
+        df = WebFlashscore.flashscore('https://www.flashscore.com', delta,  amount)
+    elif 'Basketball' == sport:
+        df = WebFlashscore.flashscore('https://www.flashscore.com/basketball/', delta,amount)
+    elif 'Handball' == sport:
+        df = WebFlashscore.flashscore('https://www.flashscore.com/handball/', delta, amount)
+    else:
+        df = 0
+    session['df'] = df.to_json(orient='records')
+    html_string = df.to_html(classes='table table-striped', index=False)
+    html_string = html_string.replace('<table',
+                                          '<table style="background-color: lightblue; border: 2px solid black;"')
+    html_string = html_string.replace('<tbody>', '<tbody style="background-color: white;">')
+
+    return render_template('tables.html', tabela=html_string, tytul='d1')
+
+
+
+
+@app.route('/some_url', methods=['POST'])
+def some_function():
+    json_data  = session.get('df')
+    df = pd.read_json(json_data)
+    df.to_csv('data.csv', index=False)
+    return 'Data saved to CSV'
 
 if __name__ == '__main__':
     app.run(debug=True)
